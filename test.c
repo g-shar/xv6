141e02f5f96252bb85c068e39849430109248b97
Estela Ramirez Ramirez <erami151@sledge.cs.ucr.edu>
2021-04-22 11:45:38 -0700

    added exit(0) to fix trap error

--- test.c
+++ test.c
@@ -8,14 +8,11 @@ int main(int argc, char** argv){
     if (pid){   // parent
         //wait(&status);    // exit status will be 123
         waitpid(pid, &status, 0);
-        printf(1, "status %d\n", status);
+        printf(1, "parent process, child status is: %d\n", status);
     }else{      // child
         printf(1, "child process\n");
         exit(123);
     }
-
-    // int waitpid(int pid, int* status, int options)
-    // exit(0);
-    return 0;
+    exit(0);
 }
 

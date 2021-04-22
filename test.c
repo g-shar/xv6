4908b7c188418b6f055a90c831a5d562965c1d71
Estela Ramirez Ramirez <erami151@sledge.cs.ucr.edu>
2021-04-21 21:31:56 -0700

    updated to test waitpid() system call

--- test.c
+++ test.c
@@ -3,11 +3,11 @@
 #include "user.h"
 
 int main(int argc, char** argv){
-    printf(1, "Hello world\n");
     int status;
     int pid = fork();
     if (pid){   // parent
-        wait(&status);    // exit status will be 123
+        //wait(&status);    // exit status will be 123
+        waitpid(pid, &status, 0);
         printf(1, "status %d\n", status);
     }else{      // child
         printf(1, "child process\n");

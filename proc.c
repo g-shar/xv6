3b27d7f8fab10133f0496a2896d16f381b21240a
Estela Ramirez Ramirez <erami151@sledge.cs.ucr.edu>
2021-05-11 11:14:53 -0700

    modified priority scheduling

--- proc.c
+++ proc.c
@@ -337,59 +337,52 @@ scheduler(void)
   struct proc *p;
   struct cpu *c = mycpu();
   c->proc = 0;
-  struct proc *p2;
+  int highestPVal = 31;
+
+  for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
+      // find the highest priority value in the processes
+      if(p->state != RUNNABLE)
+          continue;
+      if (p->prior_val < highestPVal){
+          highestPVal = p->prior_val;
+      }
+  }
 
   for(;;){
     // Enable interrupts on this processor.
     sti();
-    struct proc* highestPvalProc;
-    int highestPval = 31;
+
     // Loop over process table looking for process to run.
     acquire(&ptable.lock);
     for(p = ptable.proc; p < &ptable.proc[NPROC]; p++){
       if(p->state != RUNNABLE)
         continue;
-      highestPvalProc = p;
-      for(p2 = ptable.proc; p2 < &ptable.proc[NPROC]; p2++) {
-          // find the process with highest prior_val that is runnable
-          if (p2->state != RUNNABLE)
-              continue;
-          if (p2->prior_val < highestPval) {
-              highestPvalProc = p2;
-              highestPval = p2->prior_val;
+      if (p->prior_val == highestPVal){
+          if (p->prior_val < 31){
+              p->prior_val++;
+          }else{
+              p->prior_val = 31;
           }
-      }
-      p = highestPvalProc;
-      // Switch to chosen process.  It is the process's job
-      // to release ptable.lock and then reacquire it
-      // before jumping back to us.
-      c->proc = p;
-      switchuvm(p);
-      p->state = RUNNING;
+          // Switch to chosen process.  It is the process's job
+          // to release ptable.lock and then reacquire it
+          // before jumping back to us.
+          c->proc = p;
+          switchuvm(p);
+          p->state = RUNNING;
 
-      swtch(&(c->scheduler), p->context);
-      switchkvm();
-      if (p->prior_val < 31) {   // decrease the priority for the current process
-          p->prior_val++;
+          swtch(&(c->scheduler), p->context);
+          switchkvm();
+
+          // Process is done running for now.
+          // It should have changed its p->state before coming back.
+          c->proc = 0;
       }else{
-          p->prior_val =  31;
-      }
-
-      for(p2 = ptable.proc; p2 < &ptable.proc[NPROC]; p2++) {
-          // increase the priority for the waiting processes
-          if (p2->state != RUNNABLE)
-              continue;
-          if (p2 != p){
-              if (p2->prior_val > 0) {
-                  p2->prior_val--;
-              }else{
-                  p2->prior_val = 0;
-              }
+          if (p->prior_val > 0){
+              p->prior_val--;
+          }else{
+              p->prior_val = 0;
           }
       }
-      // Process is done running for now.
-      // It should have changed its p->state before coming back.
-      c->proc = 0;
     }
     release(&ptable.lock);
 
